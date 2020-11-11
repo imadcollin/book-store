0ed9ca9313f5380159ffb18ab350ae1c95b5da88
imadcollin <imad.collin@gmail.com>
2020-11-11 11:20:31 +0100

    Add fonts and styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       Book Store 
-      </header>
+    <div className="container">
+     
+      <div className="row">
+        <div className="col-6">one </div>
+        <div className="col-6">two</div>
+      </div>
     </div>
   );
 }
