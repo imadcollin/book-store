7b1e04e33cf7d1a1ea68b120c6d16ba0eb48602f
imadcollin <imad.collin@gmail.com>
2020-11-11 17:05:22 +0100

    Add navbar and links

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import { Switch, Route } from "react-router-dom";
 import BookList from "./Components/BookList";
 import PageNotFound from "./Components/PageNotFound";
 import Book from "./Components/Book";
+import Nav from "./Components/Nav"
 function App() {
   return (
     <React.Fragment>
+      <Nav></Nav>
       <Switch>
         <Route exact path="/" component={BookList}></Route>
         <Route path="/book" component={Book}></Route>
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,11 +1,28 @@
-import React, { Component } from 'react'
-
-export default class Nav extends Component {
-	render() {
-		return (
-			<div>
-				<h2>Nav bar </h2>
-			</div>
-		)
-	}
+import React, { Component } from "react";
+import logo from "../images/logo.svg";
+import { Navbar, Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "./AppNav.css";
+export default class AppNav extends Component {
+  render() {
+    return (
+      <Navbar bg="dark" variant="dark">
+        <Link to="/">
+          <Navbar.Brand>
+            <img
+              alt=""
+              src={logo}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{" "}
+            Book Store
+          </Navbar.Brand>
+        </Link>{" "}
+        <Nav className="mr-auto">
+          <Nav.Link href="/">Home</Nav.Link>
+        </Nav>
+      </Navbar>
+    );
+  }
 }
