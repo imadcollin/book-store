ee48a33d240c2951421c07e8abdebaa2a9cf9556
imadcollin <imad.collin@gmail.com>
2020-11-12 14:20:16 +0100

    Add some mock data

---
+++ src/Providers/MockData.js
@@ -0,0 +1,17 @@
+const bookList = [
+  { id: 1, title: "Java", Author: "J.K", img: "../images/1.jpg", price: 40 },
+  { id: 2, title: "Python", Author: "J.K", img: "../images/2.jpg", price: 60 },
+];
+
+const bookDetail = [
+  {
+    id: 1,
+    title: "Java",
+    Author: "J.K",
+    img: "../images/1.jpg",
+    price: 40,
+    info:
+      "lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem",
+  },
+];
+export {bookList,bookDetail}
