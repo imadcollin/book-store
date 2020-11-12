4e05307d9869fc7a7e4480b70d62d23b7802dc0c
imadcollin <imad.collin@gmail.com>
2020-11-12 17:00:02 +0100

    Add more mock data

--- src/Providers/MockData.js
+++ src/Providers/MockData.js
@@ -1,6 +1,9 @@
 const bookList = [
-  { id: 1, title: "Java", Author: "J.K", img: "../images/1.jpg", price: 40 },
-  { id: 2, title: "Python", Author: "J.K", img: "../images/2.jpg", price: 60 },
+  { id: 1, title: "Java", Author: "J.K", img: "images/1.jpg", price: 40 },
+  { id: 2, title: "C++", Author: "J.K", img:"images/5.jpg", price: 60 },
+  { id: 3, title: "Pascal", Author: "J.K", img:"images/6.jpg", price: 60 },
+  { id: 4, title: "Perl", Author: "J.K", img:"images/7.jpg", price: 60 },
+  { id: 5, title: "Haskell", Author: "J.K", img:"images/8.jpg", price: 60 },
 ];
 
 const bookDetail = [
@@ -8,7 +11,7 @@ const bookDetail = [
     id: 1,
     title: "Java",
     Author: "J.K",
-    img: "../images/1.jpg",
+    img: "images/1.jpg",
     price: 40,
     info:
       "lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem",
