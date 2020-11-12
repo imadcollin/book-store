2c3eb1f56cc5eef7454c4c1b8b40dfe52a6e4500
imadcollin <imad.collin@gmail.com>
2020-11-12 16:59:09 +0100

    Move images to public folder

--- src/Components/Book.js
+++ src/Components/Book.js
@@ -1,11 +1,22 @@
-import React, { Component } from 'react'
-
+import React, { Component } from "react";
+import Card from "react-bootstrap/Card";
 export default class Book extends Component {
-	render() {
-		return (
-			<div>
-				<h2>Book</h2>
-			</div>
-		)
-	}
+  render() {
+    const { title, img, author, price } = this.props.book;
+    console.log(img);
+    return (
+    
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={img}  style={{width:"100%" ,height:"200px"}}/>
+            <Card.Body>
+              <Card.Title>{title}</Card.Title>
+              <Card.Text>{price}</Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer>
+          </Card>
+      
+    );
+  }
 }
