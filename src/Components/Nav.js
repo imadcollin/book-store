22b40228b73b07eec9e8405ecca6537efae8e615
imadcollin <imad.collin@gmail.com>
2020-11-13 13:19:16 +0100

    Move log to root folder

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import logo from "../images/logo.svg";
+import logo from "../logo.svg";
 import { Navbar, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "./AppNav.css";
