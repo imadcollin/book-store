473fbf04eea7b67bab84a52b0a5a16752342a385
imadcollin <imad.collin@gmail.com>
2020-11-13 17:00:03 +0100

    Add mock details data

--- src/Providers/MockData.js
+++ src/Providers/MockData.js
@@ -1,9 +1,9 @@
 const bookList = [
   { id: 1, title: "Java", Author: "J.K", img: "images/1.jpg", price: 40 },
-  { id: 2, title: "C++", Author: "J.K", img:"images/5.jpg", price: 60 },
-  { id: 3, title: "Pascal", Author: "J.K", img:"images/6.jpg", price: 60 },
-  { id: 4, title: "Perl", Author: "J.K", img:"images/7.jpg", price: 60 },
-  { id: 5, title: "Haskell", Author: "J.K", img:"images/8.jpg", price: 60 },
+  { id: 2, title: "C++", Author: "J.K", img: "images/5.jpg", price: 60 },
+  { id: 3, title: "Pascal", Author: "J.K", img: "images/6.jpg", price: 60 },
+  { id: 4, title: "Perl", Author: "J.K", img: "images/7.jpg", price: 60 },
+  { id: 5, title: "Haskell", Author: "J.K", img: "images/8.jpg", price: 60 },
 ];
 
 const bookDetail = [
@@ -12,9 +12,41 @@ const bookDetail = [
     title: "Java",
     Author: "J.K",
     img: "images/1.jpg",
+    price: 60,
+    info:
+      "An so vulgar to on points wanted. Not rapturous resolving continued household northward gay. He it otherwise supported instantly. Unfeeling agreeable suffering it on smallness newspaper be. So come must time no as. Do on unpleasing possession as of unreserved. Yet joy exquisite put sometimes enjoyment perpetual now. Behind lovers eat having length horses vanity say had its. ",
+  },  {
+    id: 2,
+    title: "Java",
+    Author: "J.K",
+    img: "images/5.jpg",
     price: 40,
     info:
-      "lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem",
+      "An so vulgar to on points wanted. Not rapturous resolving continued household northward gay. He it otherwise supported instantly. Unfeeling agreeable suffering it on smallness newspaper be. So come must time no as. Do on unpleasing possession as of unreserved. Yet joy exquisite put sometimes enjoyment perpetual now. Behind lovers eat having length horses vanity say had its. ",
+  },  {
+    id: 3,
+    title: "Java",
+    Author: "J.K",
+    img: "images/6.jpg",
+    price: 20,
+    info:
+      "An so vulgar to on points wanted. Not rapturous resolving continued household northward gay. He it otherwise supported instantly. Unfeeling agreeable suffering it on smallness newspaper be. So come must time no as. Do on unpleasing possession as of unreserved. Yet joy exquisite put sometimes enjoyment perpetual now. Behind lovers eat having length horses vanity say had its. ",
+  },  {
+    id: 4,
+    title: "Java",
+    Author: "J.K",
+    img: "images/7.jpg",
+    price: 70,
+    info:
+      "An so vulgar to on points wanted. Not rapturous resolving continued household northward gay. He it otherwise supported instantly. Unfeeling agreeable suffering it on smallness newspaper be. So come must time no as. Do on unpleasing possession as of unreserved. Yet joy exquisite put sometimes enjoyment perpetual now. Behind lovers eat having length horses vanity say had its. ",
+  },  {
+    id: 5,
+    title: "Java",
+    Author: "J.K",
+    img: "images/8.jpg",
+    price: 90,
+    info:
+      "An so vulgar to on points wanted. Not rapturous resolving continued household northward gay. He it otherwise supported instantly. Unfeeling agreeable suffering it on smallness newspaper be. So come must time no as. Do on unpleasing possession as of unreserved. Yet joy exquisite put sometimes enjoyment perpetual now. Behind lovers eat having length horses vanity say had its. ",
   },
 ];
-export {bookList,bookDetail}
+export { bookList, bookDetail };
