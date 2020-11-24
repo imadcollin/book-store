d533c81783895be86948bbf40d563b90b7811156
imadcollin <imad.collin@gmail.com>
2020-11-24 15:57:54 +0100

    Add Auth-provider

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
-import {ProductProvider} from "./Providers/ProductProvider";
+import { ProductProvider } from "./Providers/ProductProvider";
+import AuthProvider from "./Providers/AuthProvider";
+
 ReactDOM.render(
   <ProductProvider>
     <Router>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </Router>
   </ProductProvider>,
   document.getElementById("root")
